3847c7e113b9fc2e9ef020183ebeaafef8026285
alysov <alysov@mail.ru>
2020-12-24 18:12:59 +0300

    Описать сущность Book

--- src/domain/store/Book.cs
+++ src/domain/store/Book.cs
@@ -3,6 +3,15 @@
 namespace store
 {
 	public class Book
-	{
+	{	
+		public int Id { get; }
+
+		public string Title { get; }
+
+		public Book(int id, string title)
+		{
+			Id = id;
+			Title = title;
+		}
 	}
 }
